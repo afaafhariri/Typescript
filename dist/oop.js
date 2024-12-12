58aa48706a6af6638fe6d2a240ec94ae05e37a9f
Afaaf Hariri <haririafaaf@gmail.com>
2024-12-13 00:22:59 +0530

    Inheritance

--- dist/oop.js
+++ dist/oop.js
@@ -1,4 +1,5 @@
 "use strict";
+//class
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -10,5 +11,24 @@ class Person {
 }
 const personExampleOne = new Person("Hariri", 21);
 const personExampleTwo = new Person("Messi", "37");
-console.log(personExampleOne); //Person { name: 'Hariri', age: 21 }
-console.log(personExampleTwo); //Person { name: 'Messi', age: '37' }
+//console.log(personExampleOne); //Person { name: 'Hariri', age: 21 }
+//console.log(personExampleTwo); //Person { name: 'Messi', age: '37' }
+//Inheritance & access modifiers
+class Animal {
+    constructor(species) {
+        this.species = species;
+    }
+    makeSound() {
+        return "Meowwwww";
+    }
+}
+class Cat extends Animal {
+    constructor(breed, species) {
+        super(species);
+        this.breed = breed;
+    }
+    details() {
+        return `This animal species is ${this.species} and the breed is ${this.breed}`;
+    }
+}
+const newAnimal = new Cat("Cat", "British Short Hair");
--- src/oop.ts
+++ src/oop.ts
@@ -1,3 +1,4 @@
+//class
 class Person {
   name: string;
   age: number | string;
@@ -13,5 +14,31 @@ class Person {
 
 const personExampleOne = new Person("Hariri", 21);
 const personExampleTwo = new Person("Messi", "37");
-console.log(personExampleOne); //Person { name: 'Hariri', age: 21 }
-console.log(personExampleTwo); //Person { name: 'Messi', age: '37' }
+//console.log(personExampleOne); //Person { name: 'Hariri', age: 21 }
+//console.log(personExampleTwo); //Person { name: 'Messi', age: '37' }
+
+//Inheritance & access modifiers
+class Animal {
+  protected species: string;
+
+  constructor(species: string) {
+    this.species = species;
+  }
+  public makeSound(): string {
+    return "Meowwwww";
+  }
+}
+class Cat extends Animal {
+  private breed: string;
+
+  constructor(breed: string, species: string) {
+    super(species);
+    this.breed = breed;
+  }
+
+  private details(): string {
+    return `This animal species is ${this.species} and the breed is ${this.breed}`;
+  }
+}
+
+const newAnimal = new Cat("Cat", "British Short Hair");
